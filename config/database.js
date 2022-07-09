49dd173ae11f0c4a806ef7f97d93acfc840baf45
kdan80 <kierandansey80@gmail.com>
2022-07-09 18:45:15 +0100

    Refactore env vars

--- config/database.js
+++ config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
-const {MONGODB_URI} = process.env;
-
 exports.connect = () => {
-    mongoose.connect(MONGODB_URI, {
+    mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const app = require("./app");
 // TODO: Use https.createServer for production
 const server = http.createServer(app);
 
-const { AUTHY_PORT } = process.env || 4000;
-
-server.listen(AUTHY_PORT, () => {
+server.listen(process.env.AUTHY_PORT, () => {
     //console.log(`Server running on port ${JWT_AUTH_PORT}...`);
-    process.stdout.write(`Server running on port ${AUTHY_PORT}...\n`);
+    process.stdout.write(`Server running on port ${process.env.AUTHY_PORT}...\n`);
 })

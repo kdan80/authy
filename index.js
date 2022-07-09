d9cf33fb83764d5418b0dfa133ec09b042b48f84
kdan80 <kierandansey80@gmail.com>
2022-07-09 18:51:50 +0100

    Updated env vars

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require("./app");
 // TODO: Use https.createServer for production
 const server = http.createServer(app);
 
-server.listen(process.env.AUTHY_PORT, () => {
+server.listen(process.env.PORT, () => {
     //console.log(`Server running on port ${JWT_AUTH_PORT}...`);
-    process.stdout.write(`Server running on port ${process.env.AUTHY_PORT}...\n`);
+    process.stdout.write(`Server running on port ${process.env.PORT}...\n`);
 })
